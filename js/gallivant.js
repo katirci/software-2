562517e69ece344dfba301281649ef0f5549a70e
KATIRCI <umut@wpcenter.io>
2022-08-15 19:50:56 +0300

    mobile menu refactor and search input margin fixed

---
+++ js/gallivant.js
@@ -0,0 +1,5 @@
+let element = document.querySelector(".mobile-menu-checkbox");
+console.log(element);
+let menu = documnet.querySelector(".mobile-submenu-container");
+console.log(menu);
+element.addEventListener("click")
