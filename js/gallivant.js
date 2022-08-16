b00e49061897184700f95dbf0dc37be91c3485a9
KATIRCI <umut@wpcenter.io>
2022-08-16 09:55:31 +0300

    mobile design correct and font-size change in home page article

--- js/gallivant.js
+++
@@ -1,5 +0,0 @@
-let element = document.querySelector(".mobile-menu-checkbox");
-console.log(element);
-let menu = documnet.querySelector(".mobile-submenu-container");
-console.log(menu);
-element.addEventListener("click")
