7510c30fe335b6c0d6683f82e74af0fbc2ee3397
KATIRCI <umut@wpcenter.io>
2022-08-17 14:33:18 +0300

    the category page of the project create

--- js/gallivant.js
+++ js/gallivant.js
@@ -1,13 +1,42 @@
+var swiper = new Swiper(".mySwiper", {
+  
+  spaceBetween: 30,
+  
+  loop: true,
+  loopFillGroupWithBlank: false,
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1300:{
+      slidesPerView: 6,
+      slidesPerGroup: 6,
+    },
+    1100:{
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+      spaceBetween: 15,
+    },
+    900:{
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+    700:{
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    540:{
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    
+  }
+});
 
-      var swiper = new Swiper(".mySwiper", {
-        cssMode: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-        },
-        mousewheel: true,
-        keyboard: true,
-      });
